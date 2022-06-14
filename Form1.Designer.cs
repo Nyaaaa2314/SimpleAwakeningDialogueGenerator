84efb409282cff2e651754d2aa6ddd5ea48a174e
Nyaaaa2314 <61416841+Nyaaaa2314@users.noreply.github.com>
2022-06-14 03:17:34 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace AwakeningDialogueWriter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.add = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.args = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1036, 48);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(326, 390);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(699, 48);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(331, 390);
+            this.add.TabIndex = 1;
+            this.add.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "SetSpeaker",
+            "Emotion",
+            "Wait",
+            "FadeOut",
+            "FadeIn",
+            "Pause",
+            "Clear"});
+            this.listBox1.Location = new System.Drawing.Point(148, 209);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(161, 134);
+            this.listBox1.TabIndex = 2;
+            // 
+            // args
+            // 
+            this.args.Location = new System.Drawing.Point(347, 209);
+            this.args.Name = "args";
+            this.args.Size = new System.Drawing.Size(100, 20);
+            this.args.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "AddCommand";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(612, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Add Text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(612, 295);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Delete Line";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(699, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Text To Add";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1036, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Dialogue Script Output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(344, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Args";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(148, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Initialize";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(148, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(272, 21);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(612, 132);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Clear output";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(612, 103);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "Clear input";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1374, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.args);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox add;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox args;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AwakeningDialogueWriter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(richTextBox1.TextLength == 0)
+            {
+                return;
+            }
+            else
+            {
+                richTextBox1.Lines = richTextBox1.Lines.Take(richTextBox1.Lines.Length - 1).ToArray();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItem == null || args.Text == "")
+            {
+                return;
+            }
+            else
+            {
+                switch (listBox1.SelectedIndex)
+                {
+                    case 0:
+                        if(args.TextLength == 0)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            richTextBox1.AppendText("\n$SetSpeaker(" + args.Text + ")\n$Synchronize");
+
+                        }
+                        break;
+                    case 1:
+                        if (args.TextLength == 0)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            richTextBox1.AppendText("\n$Emotion(" + args.Text + ")");
+
+                        }
+                        break;
+                    case 2:
+                        if (args.TextLength == 0)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            richTextBox1.AppendText("\n$Wait(" + args.Text + ")");
+
+                        }
+                        break;
+                    case 3:
+                        if (args.TextLength == 0)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            richTextBox1.AppendText("\n$FadeOut(" + args.Text + ")");
+
+                        }
+                        break;
+                    case 4:
+                        if (args.TextLength == 0)
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            richTextBox1.AppendText("\n$FadeIn(" + args.Text + ")");
+
+                        }
+                        break;
+                    case 5:
+                        richTextBox1.AppendText("\n$Pause");
+                        break;
+                    case 6:
+                        richTextBox1.AppendText("\n$Clear");
+                        break;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(textBox2.TextLength == 0 && textBox3.TextLength == 0)
+            {
+                return;
+            }
+            else
+            {
+                richTextBox1.AppendText("$SetConversationType(1)\n" + "$LoadAssets(" + textBox2.Text + ", " + "3)\n"
+                    + "$Wait(0)\n$LoadAssets(" + textBox3.Text + ", " + "7)");
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(add.TextLength == 0)
+            {
+                return;
+            }
+            else
+            {
+                string[] words = add.Text.Split(' ');
+                int i = 0;
+                int j = 43;
+                richTextBox1.AppendText("\n");
+                foreach(string s in words){
+                    if(j - s.Length >= 0)
+                    {
+                        richTextBox1.AppendText(s + ' ');
+                        j -= s.Length;
+                    }
+                    else
+                    {
+                        i++;
+                        if(i == 2)
+                        {
+                            richTextBox1.AppendText("\n$Pause\n$Clear");
+                        }
+                        j = 43;
+                        richTextBox1.AppendText("\n" + s + ' ');
+                        j -= s.Length;
+                    }
+                }
+                richTextBox1.AppendText("\n$Pause");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            add.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+    }
+}
